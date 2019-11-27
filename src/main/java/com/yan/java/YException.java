38880c9f1fd91926a0faed69093e01458545a077
yanxujiang <yanxujiang@sensorsdata.cn>
2019-11-27 21:07:43 +0800

    添加两种异常(检查/非检查)实现

--- src/main/java/com/yan/java/YException.java
+++ src/main/java/com/yan/java/YException.java
@@ -2,8 +2,14 @@ package com.yan.java;
 
 /**
  * Created by yanxujiang on 2019-11-27.
+ * 检查异常，也称非运行时异常，非 RuntimeException 及其子类 以及 Error ，其他都是检查异常
+ * 检查异常，意味着编译器要求开发者必须要处理，方法必须 throws exception 或者 try catch
+ * 注意，由于 java 要求子类抛出的异常不能超过父类抛出异常的范围。
+ * 通常如果父类方法不抛出异常，创建匿名子类编写逻辑时，要抛出异常，往往只能通过 try catch 实现
+ * 检查异常往往都是程序语法异常，不处理，无法编译通过
  */
 public class YException extends Exception {
+
   public YException() {
     super();
   }
--- src/main/java/com/yan/java/YRuntimeException.java
+++ src/main/java/com/yan/java/YRuntimeException.java
@@ -2,6 +2,10 @@ package com.yan.java;
 
 /**
  * Created by yanxujiang on 2019-11-27.
+ * 非检查异常,也称运行时异常， RuntimeException 及其子类 以及 Error
+ *
+ * 常常是代码中出现逻辑问题，抛出非检查异常
+ * 如 NullPointerException IndexOutOfBoundException
  */
 public class YRuntimeException extends RuntimeException {
   public YRuntimeException() {
